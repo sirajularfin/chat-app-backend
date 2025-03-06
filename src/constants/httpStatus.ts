52782cafe57ca563a972c1b977a10b2d20da121f
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-06 17:46:09 +0530

    feat: add contants

---
+++ src/constants/httpStatus.ts
@@ -0,0 +1,3 @@
+export const HTTP_STATUS_OK = 200;
+export const HTTP_STATUS_CREATED = 201;
+export const HTTP_STATUS_BAD_REQUEST = 400;
---
+++ src/constants/routes.ts
@@ -0,0 +1,5 @@
+export const ROUTE_ROOT = '/';
+export const ROUTE_LOGIN = '/login';
+export const ROUTE_REGISTER = '/register';
+export const ROUTE_HOME = '/home';
+export const ROUTE_USER = '/user';
