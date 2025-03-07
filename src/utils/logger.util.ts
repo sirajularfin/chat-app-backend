c4e1de8a767f507f91218f042db3b12ae6632621
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-07 15:54:15 +0530

    update: logger

--- src/utils/logger.util.ts
+++ src/utils/logger.util.ts
@@ -3,19 +3,19 @@ import moment from 'moment';
 const logger = (error: string, level: 'info' | 'log' | 'warn' | 'error' = 'log'): void => {
 	switch (level) {
     case 'info':
-      console[level](`[INFO][${moment().format('HH:mm:ss')}] ${error}`);
+      console[level](`ℹ️  [INFO][${moment().format('HH:mm:ss')}] ${error}`);
       break;
 
     case 'log':
-      console[level](`[LOG][${moment().format('HH:mm:ss')}] ${error}`);
+      console[level](`🪵 [LOG][${moment().format('HH:mm:ss')}] ${error}`);
       break;
 
     case 'warn':
-      console[level](`[WARN][${moment().format('HH:mm:ss')}] ${error}`);
+      console[level](`⚠️ [WARN][${moment().format('HH:mm:ss')}] ${error}`);
       break;
 
     case 'error':
-      console[level](`[ERROR][${moment().format('HH:mm:ss')}] ${error}`);
+      console[level](`🚨 [ERROR][${moment().format('HH:mm:ss')}] ${error}`);
   }
 };
 
