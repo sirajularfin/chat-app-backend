460414c4dd4317f0fdf81e15d960b6105a20fbef
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-07 15:56:06 +0530

    update: app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,24 +2,27 @@ import express from 'express';
 import userRoutes from './routes/userRoutes.js';
 import { formatJson } from './utils/commons.util.js';
 import logger from './utils/logger.util.js';
+import { loadConfig } from './utils/config.util.js';
+import connectDb from './integrations/database/index.js';
 
 const app = express();
-// TODO: Fetch PORT from .env files
-const port = 3000;
+const { port } = await loadConfig();
 
 app.use(express.json()); // Parses JSON request bodies
 app.use(express.urlencoded({ extended: true })); // Parses form data (x-www-form-urlencoded)
 
 app.use((req, res, next) => {
-  logger(`[ClientRequest] Body: ${formatJson(req.body)}`);
+  logger(`[ApiRequest] Body: ${formatJson(req.body)}`);
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   next();
-}); 
+});
 
 app.use('/ums', userRoutes);
 
-app.listen(port, () => {
-  logger(`Server is running on port: ${port}`);
+await connectDb(client => {
+  app.listen(port, () => {
+    logger(`[Server] Started at port: ${port}`, 'info');
+  });
 });
