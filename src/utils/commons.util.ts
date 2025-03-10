0c6d78c541742d788a0d66af94cd574c039c2992
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-10 14:23:20 +0530

    modify: remove requestValidation middleware, and instead use it as util function.

--- src/utils/commons.util.ts
+++ src/utils/commons.util.ts
@@ -1,12 +1,28 @@
+import { Request, Response } from 'express';
 import logger from './logger.util.js';
+import { validationResult } from 'express-validator';
+import HttpStatus from '../constants/httpStatus.js';
 
 export const formatJson = (args: undefined): string => {
-	try {
+  try {
     const jsonString = typeof args === 'string' ? args : JSON.stringify(args);
     const parsed = JSON.parse(jsonString);
     return JSON.stringify(parsed, null, 2);
   } catch (e) {
     logger(`[JSON Formatting] Invalid JSON: ${e}`, 'error');
     return String(args);
+  }
+};
+
+export const checkIfPostOrPutRequest = (req: Request): boolean => {
+  return req.method === 'POST' || req.method === 'PUT';
+};
+
+export const checkRequestErrors = (req: Request, res: Response) => {
+  if (checkIfPostOrPutRequest(req)) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.status(HttpStatus.BAD_REQUEST).json({ errors: errors.array() });
+    }
   }
 };
