193255e2ada114f11557ecbb13da1cbb887009fa
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-10 15:50:52 +0530

    update: user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from 'express';
-import { ROUTE_LOGIN, ROUTE_REGISTER, ROUTE_USER } from '../constants/routes.js';
+import { ROUTE_FIND, ROUTE_FIND_BY_ID, ROUTE_REGISTER } from '../constants/routes.js';
 import * as userController from '../controllers/user.controller.js';
 import registrationFormValidator from '../validators/userRegistration.validator.js';
 
 const router = Router();
 
-router.post(ROUTE_LOGIN, userController.loginUser);
 router.post(ROUTE_REGISTER, registrationFormValidator, userController.registerUser);
-router.get(ROUTE_USER, userController.getAllUsers);
+router.get(ROUTE_FIND, userController.getAllUsers);
+router.get(ROUTE_FIND_BY_ID, userController.getUserById);
 
 export default router;
