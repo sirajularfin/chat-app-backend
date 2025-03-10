71a684694b5b335a4a9be18c18bfcfc2824ec6cd
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-10 14:23:42 +0530

    feat: user schema

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from 'mongoose';
+
+/**
+ * Schema for the user collection
+ */
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('User', userSchema);
