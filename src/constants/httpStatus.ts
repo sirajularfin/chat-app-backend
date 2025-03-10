ccfcbd8fa4ee2a716544aeb463ac7d2a49d48f2d
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-10 13:44:48 +0530

    refactor: create httpStatus enum

--- src/constants/httpStatus.ts
+++ src/constants/httpStatus.ts
@@ -1,3 +1,8 @@
-export const HTTP_STATUS_OK = 200;
-export const HTTP_STATUS_CREATED = 201;
-export const HTTP_STATUS_BAD_REQUEST = 400;
+enum HttpStatus {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  INTERNAL_SERVER_ERROR = 500,
+}
+
+export default HttpStatus;
