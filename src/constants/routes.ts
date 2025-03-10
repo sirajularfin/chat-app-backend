12ac3741168583a79c9e0edb0b96943944bd01fe
Sirajul Arfin <sirajularfin@gmail.com>
2025-03-10 14:27:55 +0530

    refactor: routes

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -1,5 +1,6 @@
 export const ROUTE_ROOT = '/';
-export const ROUTE_LOGIN = '/login';
-export const ROUTE_REGISTER = '/register';
-export const ROUTE_HOME = '/home';
-export const ROUTE_USER = '/user';
+export const ROUTE_ALL = ROUTE_ROOT + 'all';
+export const ROUTE_HOME = ROUTE_ROOT + 'home';
+export const ROUTE_LOGIN = ROUTE_ROOT + 'login';
+export const ROUTE_REGISTER = ROUTE_ROOT + 'register';
+export const ROUTE_USER = ROUTE_ROOT + 'user';
